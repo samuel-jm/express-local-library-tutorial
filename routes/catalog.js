62d92e02f89667559cdf27ed0ae7bb98319eb1c1
samuel-jm <114629227+samuel-jm@users.noreply.github.com>
2024-05-10 12:05:44 +1200

    Undo accidental route deletion

--- routes/catalog.js
+++ routes/catalog.js
@@ -25,7 +25,16 @@ router.get("/authors", author_controller.author_list);
 router.get("/author/create", author_controller.author_create_get);
 router.post("/author/create", author_controller.author_create_post);
 router.get("/author/:id/delete", author_controller.author_delete_get);
-router.post("/authataloge/create", genre_controller.genre_create_post);
+router.post("/author/:id/delete", author_controller.author_delete_post);
+router.get("/author/:id/update", author_controller.author_update_get);
+router.post("/author/:id/update", author_controller.author_update_post);
+router.get("/author/:id", author_controller.author_detail);
+
+/// GENRE ROUTES ///
+
+router.get("/genres", genre_controller.genre_list);
+router.get("/genre/create", genre_controller.genre_create_get);
+router.post("/genre/create", genre_controller.genre_create_post);
 router.get("/genre/:id/delete", genre_controller.genre_delete_get);
 router.post("/genre/:id/delete", genre_controller.genre_delete_post);
 router.get("/genre/:id/update", genre_controller.genre_update_get);
