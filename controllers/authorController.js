1efdff23f7b28fbd408b8c60d07258988be9b092
samuel-jm <114629227+samuel-jm@users.noreply.github.com>
2024-05-13 11:03:41 +1200

    Implement the create author form

--- controllers/authorController.js
+++ controllers/authorController.js
@@ -1,3 +1,4 @@
+const { body, validationResult } = require("express-validator");
 const Author = require("../models/author");
 const Book = require("../models/book");
 
@@ -40,12 +41,56 @@ exports.author_detail = async (req, res, next) => {
 };
 
 exports.author_create_get = (req, res, next) => {
-  res.send("TODO: Author create GET");
+  res.render("authorForm", { title: "Create Author" });
 };
 
-exports.author_create_post = (req, res, next) => {
-  res.send("TODO: Author create POST");
-};
+exports.author_create_post = [
+  body("first_name")
+    .trim()
+    .isLength({ min: 1 })
+    .escape()
+    .withMessage("First name must be specified.")
+    .isAlphanumeric()
+    .withMessage("First name has non-alphanumeric characters."),
+  body("family_name")
+    .trim()
+    .isLength({ min: 1 })
+    .escape()
+    .withMessage("Family name must be specified.")
+    .isAlphanumeric()
+    .withMessage("Family name has non-alphanumeric characters."),
+  body("date_of_birth")
+    .optional({ values: "falsy" })
+    .isISO8601()
+    .withMessage("Date should be in the form dd/mm/yyyy.")
+    .toDate(),
+  body("date_of_death")
+    .optional({ values: "falsy" })
+    .isISO8601()
+    .withMessage("Date should be in the form dd/mm/yyyy.")
+    .toDate(),
+  async (req, res, next) => {
+    const errors = validationResult(req);
+
+    const author = new Author({
+      first_name: req.body.first_name,
+      family_name: req.body.family_name,
+      date_of_birth: req.body.date_of_birth,
+      date_of_death: req.body.date_of_death,
+    });
+
+    if (!errors.isEmpty()) {
+      res.render("authorForm", {
+        title: "Create Author",
+        author: author,
+        errors: errors.array(),
+      });
+    } else {
+      await author.save();
+      res.redirect(author.url);
+    }
+  },
+];
 
 exports.author_delete_get = (req, res, next) => {
   res.send("TODO: Author delete GET");
